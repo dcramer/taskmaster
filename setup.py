32ad432156d7ea51f4e880f551a571828943ab54
David Cramer <dcramer@gmail.com>
2012-11-06 14:02:47 -0800

    0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="taskmaster",
     license='Apache License 2.0',
-    version="0.6.0",
+    version="0.7.0",
     description="",
     author="David Cramer",
     author_email="dcramer@gmail.com",
